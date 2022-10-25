c652f7360cc6d29d9fcc4191a9c2e6980930a169
gabrielraeder <gabrielraeder@outlook.com.br>
2022-10-25 17:29:43 -0300

    requisito 07

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT
+	  A.nome AS artista,
+	  AL.nome AS album,
+      COUNT(UA.artist_id) AS seguidores
+      FROM SpotifyClone.albuns AS AL
+      INNER JOIN SpotifyClone.artist AS A ON AL.artist_id = A.artist_id
+      INNER JOIN SpotifyClone.user_artist AS UA ON AL.artist_id = UA.artist_id
+      GROUP BY AL.album_id
+      ORDER BY seguidores DESC, artista, album;
+      
